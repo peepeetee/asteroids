bb7819b596368faa3e69d6c285ae605549792a46
peepeetee <peepeetee@protonmail.com>
2025-04-28 02:34:39 +0900

    cap framerate to 60

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +20,7 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+        clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
