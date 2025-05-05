3cbcae63af2fc17b4ceaed0c2fd70d8d40ba7d83
peepeetee <peepeetee@protonmail.com>
2025-05-05 10:08:00 +0800

    add progress: asteroids and shooting

--- main.py
+++ main.py
@@ -3,7 +3,14 @@
 # throughout this file
 import pygame
 
+import sys
+
 from constants import *
+
+from player import Player
+from shot import Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main(): 
     print("Starting Asteroids!")
@@ -13,13 +20,45 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroidfield = AsteroidField()
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
+
     while 1:
+        #handle game exit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        #draw screen
         screen.fill("black")
 
+        #update objects
+        updatable.update(dt)
+
+        for item in asteroids:
+            if player.collision_detection(item):
+                print("Game over!")
+                sys.exit()
+
+        #draw things
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
+        #cap framerate to 60hz
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
